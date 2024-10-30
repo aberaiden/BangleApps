bdd00367b206b208a80b82afafa4c393ffce8029
aberaiden <abe@raiden.se>
2024-10-30 16:21:23 +0100

    Update app.js

--- apps/worldclock/app.js
+++ apps/worldclock/app.js
@@ -1,73 +1,23 @@
-const big = g.getWidth()>200;
 // Font for primary time and date
-const primaryTimeFontSize = big?6:5;
-const primaryDateFontSize = big?3:2;
-
-// Font for single secondary time
-const secondaryTimeFontSize = 4;
-const secondaryTimeZoneFontSize = 2;
-
-// Font / columns for multiple secondary times
+const primaryTimeFontSize = 5;
+const primaryDateFontSize = 2;
 const secondaryRowColFontSize = 2;
+
 const xcol1 = 10;
 const xcol2 = g.getWidth() - xcol1;
 
 const font = "6x8";
 
-/* TODO: we could totally use 'Layout' here and
-avoid a whole bunch of hard-coded offsets */
-
-
 const xyCenter = g.getWidth() / 2;
-const yposTime = big ? 75 : 60;
-const yposTime2 = yposTime + (big ? 100 : 60);
-const yposDate = big ? 130 : 90;
-const yposWorld = big ? 170 : 120;
-
-const yposWeek = big ? 140 : 100;
+const yposTime = 60;
+const yposDate = 90;
+const yposWorld = 120;
+const yposWeek = 105;
 
 const OFFSET_TIME_ZONE = 0;
 const OFFSET_HOURS = 1;
 
 var offsets = require("Storage").readJSON("worldclock.settings.json") || [];
-
-// TESTING CODE
-// Used to test offset array values during development.
-// Uncomment to override secondary offsets value
-/*
-const mockOffsets = {
- zeroOffsets: [],
- oneOffset: [["UTC", 0]],
- twoOffsets: [
-   ["Tokyo", 9],
-   ["UTC", 0],
- ],
- fourOffsets: [
-   ["Tokyo", 9],
-   ["UTC", 0],
-   ["Denver", -7],
-   ["Miami", -5],
- ],
- fiveOffsets: [
-   ["Tokyo", 9],
-   ["UTC", 0],
-   ["Denver", -7],
-   ["Chicago", -6],
-   ["Miami", -5],
-   ],
-};*/
-
-// Uncomment one at a time to test various offsets array scenarios
-//offsets = mockOffsets.zeroOffsets; // should render nothing below primary time
-//offsets = mockOffsets.oneOffset; // should render larger in two rows
-//offsets = mockOffsets.twoOffsets; // should render two in columns
-//offsets = mockOffsets.fourOffsets; // should render in columns
-//offsets = mockOffsets.fiveOffsets; // should render first four in columns
-
-// END TESTING CODE
-
-// Check settings for what type our clock should be
-//var is12Hour = (require("Storage").readJSON("setting.json",1)||{})["12hour"];
 
 // timeout used to update every minute
 var drawTimeout;
@@ -119,9 +69,10 @@ function draw() {
   var dayweek = require("locale").dow(d, 1)
   var day = d.getDate();
   var date = [dayweek, month, day].join(" ");
+  var week = "Week ".getWeek();
   g.setFont(font, primaryDateFontSize);
   g.drawString(date, xyCenter, yposDate, true);
-  g.drawString('Test 123', xyCenter, yposWeek, true);
+  g.drawString(week, xyCenter, yposWeek, true);
 
   // set gmt to UTC+0
   var gmt = new Date(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
@@ -130,30 +81,16 @@ function draw() {
   offsets.forEach((offset, index) => {
     const dx = getCurrentTimeFromOffset(gmt, offset[OFFSET_HOURS]);
     var time = require("locale").time(dx, 1);
-
-    if (offsets.length === 1) {
-      // For a single secondary timezone, draw it bigger and drop time zone to second line
-      g.setFont(font, secondaryTimeFontSize);
-      g.drawString(time, xyCenter, yposTime2, true);
-      g.setFont(font, secondaryTimeZoneFontSize);
-      g.drawString(offset[OFFSET_TIME_ZONE], xyCenter, yposTime2 + 30, true);
-
-      // draw Day, name of month, Date
-      g.setFont(font, secondaryTimeZoneFontSize);
-      g.drawString(date, xyCenter, yposDate, true);
-    } else if (index < 4) {
-      // For > 1 extra timezones, render as columns / rows
-      g.setFont(font, secondaryRowColFontSize);
-      g.setFontAlign(-1, 0);
-      g.drawString(
-        offset[OFFSET_TIME_ZONE],
-        xcol1,
-        yposWorld + index * 15,
-        true
-      );
-      g.setFontAlign(1, 0);
-      g.drawString(time, xcol2, yposWorld + index * 15, true);
-    }
+    g.setFont(font, secondaryRowColFontSize);
+    g.setFontAlign(-1, 0);
+    g.drawString(
+      offset[OFFSET_TIME_ZONE],
+      xcol1,
+      yposWorld + index * 15,
+      true
+    );
+    g.setFontAlign(1, 0);
+    g.drawString(time, xcol2, yposWorld + index * 15, true);
   });
 
   queueDraw();
